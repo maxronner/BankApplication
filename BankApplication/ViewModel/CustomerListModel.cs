ac593b483b48e46ed94fd49c55d11dd5f5b7585d
Max Ronner <max@ronner.dev>
2019-11-05 10:13:24 +0100

    Created AccountLogic again

--- BankApplication/ViewModel/CustomerListModel.cs
+++ BankApplication/ViewModel/CustomerListModel.cs
@@ -9,23 +9,12 @@ namespace BankApplication.ViewModels
 {
     public class CustomerListModel
     {
-        public ObservableCollection<Customer> Customers { get; set; }
+        public ObservableCollection<Customer> Customers { get { return CustomerLogic.Customers; } }
         public int CurrentCustomer { get; set; }
 
         public CustomerListModel()
         {
-            CustomerLogic.Customers.Add(new Customer(1234567890, "Max"));
-            CustomerLogic.Customers.Add(new Customer(1234567890, "Max"));
-            CustomerLogic.Customers.Add(new Customer(1234567890, "Max"));
-            CustomerLogic.Customers.Add(new Customer(1234567890, "Max"));
-            CustomerLogic.Customers.Add(new Customer(1234567890, "Max"));
-            CustomerLogic.Customers.Add(new Customer(1234567890, "Max"));
-            Customers = new ObservableCollection<Customer>(CustomerLogic.Customers);
         }
 
-        public Customer Current
-        {
-            get => this.Customers.Count > 0 ? this.Customers[CurrentCustomer] : null;
-        }
     }
 }
